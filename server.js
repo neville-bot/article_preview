3c3d519491e5b4b24a9fbbe0987b64a7f1ff78da
John Nevin <jnevin17@gmail.com>
2020-09-08 17:11:21 -0500

    added mobile styling and added flexibility to desktop

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log(`Listening on port ${process.env.PORT}`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
