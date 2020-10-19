452c990c36d0c279270db69d16b89e1866e1f369
John Nevin <jnevin17@gmail.com>
2020-10-19 10:14:04 -0500

    added active state for social buttons

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile("index.html");
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
